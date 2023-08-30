03bba99e515efaaf2dfd488411300fd2d3f978ce
Anastasia Zimina <anastasba@gmail.com>
2023-08-30 14:57:11 -0700

    updating jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,25 +5,49 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
-    //Rigidbody2D playerRB;
+    Rigidbody2D playerRB;
+    Animator playerAnimator;
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        // playerRB = GetComponent<Rigidbody2D>();
+        playerRB = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Run();   
+        Run(); 
+        FlipSprite();  
     }
 
     void OnMove(InputValue value) {
         moveInput = value.Get<Vector2>();
     }
 
+    void OnJump(InputValue value) {
+        if (value.isPressed) {
+            playerRB.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
     void Run() {
-       // rb.velocity = new Vector2(moveInput.x * 5f, rb.velocity.y);
-    //    rb.velocity = moveInput;
+        Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, playerRB.velocity.y);
+       playerRB.velocity = playerVelocity;
+       if (Mathf.Abs(playerRB.velocity.x) > Mathf.Epsilon) {
+           playerAnimator.SetBool("isRunning", true);
+       } else {
+           playerAnimator.SetBool("isRunning", false);
+       }
+    }
+
+
+    void FlipSprite() {
+        bool playerHasHorizontalSpeed = Mathf.Abs(playerRB.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed) {
+            transform.localScale = new Vector2(Mathf.Sign(playerRB.velocity.x), 1f);
+        }
     }
 }
